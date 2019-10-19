5ef953f2be1d1160e31d01d490ae8bd4394c2589
sizer <shinse.tanaka@gmail.com>
2019-10-19 20:47:48 +0900

    add LeanCanvasPage
    `yarn run ng g component modules/startup-tools/pages/LeanCanvasPage
    --skip-tests`

---
+++ web/src/app/modules/startup-tools/pages/lean-canvas-page/lean-canvas-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lean-canvas-page',
+  templateUrl: './lean-canvas-page.component.html',
+  styleUrls: ['./lean-canvas-page.component.sass']
+})
+export class LeanCanvasPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
