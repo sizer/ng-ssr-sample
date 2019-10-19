adf56888298916490851f48db6de0a25a121346c
sizer <shinse.tanaka@gmail.com>
2019-10-19 20:48:15 +0900

    routing startup-tools modules

--- web/src/app/modules/startup-tools/startup-tools-routing.module.ts
+++ web/src/app/modules/startup-tools/startup-tools-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LeanCanvasPageComponent } from './pages/lean-canvas-page/lean-canvas-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'canvas',
+  component: LeanCanvasPageComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- web/src/app/modules/startup-tools/startup-tools.module.ts
+++ web/src/app/modules/startup-tools/startup-tools.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { StartupToolsRoutingModule } from './startup-tools-routing.module';
+import { LeanCanvasPageComponent } from './pages/lean-canvas-page/lean-canvas-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LeanCanvasPageComponent],
   imports: [
     CommonModule,
     StartupToolsRoutingModule
