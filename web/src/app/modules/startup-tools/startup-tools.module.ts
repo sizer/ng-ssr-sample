7d2e3cfe4498d4dae49a19227e92ca09060bbcbf
sizer <shinse.tanaka@gmail.com>
2019-10-19 20:39:47 +0900

    add module as StartupTools
    `yarn run ng generate module modules/StartupTools --routing`

---
+++ web/src/app/modules/startup-tools/startup-tools.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { StartupToolsRoutingModule } from './startup-tools-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    StartupToolsRoutingModule
+  ]
+})
+export class StartupToolsModule { }
